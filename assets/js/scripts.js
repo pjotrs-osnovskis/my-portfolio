f6d38bca90f4c0e1b166083cab069c9b03ba3b65
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-09-10 22:25:05 +0100

    Add home section with blob, header, sub-heading, paragraph, contact me link and scroll down button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@ const navMenu = document.getElementById('nav-menu'),
     navClose = document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
-/* Validate if constant exists */
+/* Check if navToggle is present and add show-menu class to open navigation menu */
 if (navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
@@ -12,7 +12,7 @@ if (navToggle){
 }
 
 /*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+/* Check if navClose is present and remove show-menu class to close navigation menu */
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
@@ -24,7 +24,7 @@ const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
-    // When any nav__link is clicked, it removes show-menu class
+    // When any nav__link is clicked, it removes show-menu class to close navigation menu
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))

faf6442866d7e6ad6a727d398fbe2810dcfb09e3
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-09-10 21:54:30 +0100

    Add function to remove navigation menu when any link is clicked

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,11 +17,17 @@ if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
-
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When any nav__link is clicked, it removes show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
 

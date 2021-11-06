fd057edf9d9dcb72d399f9aa9294134766019522
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-11-06 18:32:35 +0000

    Add contact form, emailJS, styling. Update background color

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -83,7 +83,30 @@ let swiper = new Swiper(".portfolio__container", {
     },
   });
 
-/*==================== TESTIMONIAL ====================*/
+/*==================== SEND MESSAGE ====================*/
+const EmailJSUserID = "user_DoOnYBoOadoZVbGED61tp";
+const EmailJSAccessToken = "cfda4527438f42ac589a4a113c1eeb6d";
+
+function sendMail(contactForm) {
+    emailjs.init(EmailJSUserID);
+    emailjs.send("gmail", "temp_1", {
+            "from_name": contactForm.contact_name.value,
+            "from_email": contactForm.contact_email.value,
+            "project_request": contactForm.contact_request.value
+        })
+        .then(
+            function () {
+                alert("Email sent successfully");
+                document.querySelector("#contactForm").reset();
+            },
+            function (error) {
+                alert("There was an error, please try again!")
+                console.log("FAILED", error);
+            }
+        );
+
+    return false; // To block from loading a new page
+}
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
